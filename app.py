97f9dd3f2401046e056aa1eb3ac1647153e9e803
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-05-27 18:12:48 -0300

    Move funções auxiliares para utils.py, limpando o código de app.py e separando responsabilidades

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 from flask import Flask
 import json
-from datetime import datetime
+from utils import write_log, get_data
 
 app = Flask(__name__)
 
 blocked = json.loads(open('blocked.json').read())
-
-def get_json_log():
-    with open("log.json", "r") as f:
-        log = json.load(f)
-    return log
-
-def write_log(timestamp, url, status):
-    log = get_json_log()
-    data_log = {
-        "timestamp": timestamp,
-        "url": url,
-        "action": status
-    }
-    log.append(data_log)
-    with open("log.json", "w") as f:
-        json.dump(log, f, indent=3)
 
 @app.route('/')
 def hello_world():
@@ -37,12 +21,6 @@ def pegar_url(url):
     else:
         write_log(get_data(), url, "allowed")
         return f"<p>{url}</p>"
-
-def get_data():
-    data = datetime.now()
-    data = f"{data.year}-{data.month}-{data.day} {data.hour}:{data.minute}:{data.second}"
-
-    return data
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
