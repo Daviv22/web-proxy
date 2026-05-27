22b8830dd5cdac7285762c3a6178080fb2117853
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-05-27 17:34:06 -0300

    Move funções auxiliares para utils.py, limpando o código de app.py e separando responsabilidades

---
+++ utils.py
@@ -0,0 +1,23 @@
+import json
+from datetime import datetime
+
+def get_json_log():
+    with open("log.json", "r") as f:
+        log = json.load(f)
+    return log
+
+def write_log(timestamp, url, status):
+    log = get_json_log()
+    data_log = {
+        "timestamp": timestamp,
+        "url": url,
+        "action": status
+    }
+    log.append(data_log)
+    with open("log.json", "w") as f:
+        json.dump(log, f, indent=3)
+
+def get_data():
+    data = datetime.now()
+    data = f"{data.year}-{data.month}-{data.day} {data.hour}:{data.minute}:{data.second}"
+    return data
