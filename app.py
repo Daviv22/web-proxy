eb20b1f9f72a9c4fe0183d3e3ddd045ee96772bf
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-05-27 17:25:17 -0300

    Adiciona função para escrita de log

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
 blocked = json.loads(open('blocked.json').read())
+
+def get_json_log():
+    with open("log.json", "r") as f:
+        log = json.load(f)
+    return log
+
+def write_log(timestamp, url, status):
+    log = get_json_log()
+    data_log = {
+        "timestamp": timestamp,
+        "url": url,
+        "action": status
+    }
+    log.append(data_log)
+    with open("log.json", "w") as f:
+        json.dump(log, f, indent=3)
 
 @app.route('/')
 def hello_world():
@@ -15,9 +32,17 @@ def hello_world():
 @app.route('/<url>')
 def pegar_url(url):
     if url in blocked["blocked"]:
+        write_log(get_data(), url, "blocked")
         return f'<p>Site banido</p>'
     else:
+        write_log(get_data(), url, "allowed")
         return f"<p>{url}</p>"
+
+def get_data():
+    data = datetime.now()
+    data = f"{data.year}-{data.month}-{data.day} {data.hour}:{data.minute}:{data.second}"
+
+    return data
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
